47e4082c23688fa680968e13cc2a0d157c4cb8e7
RaviVarma06 <ravivarmamalladi31@gmail.com>
2024-12-24 15:12:09 +0530

    Update RemoveWord.java

--- RemoveWord.java
+++ RemoveWord.java
@@ -1,12 +1,1 @@
-package strings.com;
-
-public class RemoveWord {
-	public static void main(String[] args) {
-		String str="Exercises Practice Solution";
-		String str1="Practice";
-		String result=str.replace(str1, "");
-		System.out.println(result);
-		
-	}
-
-}
+this file is edited now
